f4c7008c7777cee9551ce7dc95eb32bab9c9c040
bsruthimala <60211273+bsruthimala@users.noreply.github.com>
2020-02-20 18:20:00 +0530

    Add files via upload

---
+++ House.java
@@ -0,0 +1,8 @@
+package com.epam.HouseConstruction;
+	public class House extends Construction{
+
+		public static void main(String[] args) {
+		House hs = new House();
+		hs.dimensions();
+		}
+}
---
+++ SimpleAndCompoundInt.java
@@ -0,0 +1,7 @@
+package com.epam.HouseConstruction;
+public class SimpleAndCompoundInt extends Calculate {
+		public static void main(String[] args) {
+		SimpleAndCompoundInt sac = new SimpleAndCompoundInt();
+		sac.details();
+		}
+	}
